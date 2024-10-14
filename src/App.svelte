519585c49b57501a1470cdc1b24e923e686ad3f2
IlkoArboit <ilkoarboit@gmail.com>
2024-10-13 23:58:59 -0300

    Code refactor

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,28 @@
 <script>
-	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <!-- svelte-ignore a11y-media-has-caption -->
+  <!-- svelte-ignore missing-declaration -->
+  <video id="player1" class="player" {src} autoplay controls></video>
+  <!-- svelte-ignore a11y-media-has-caption -->
+  <!-- svelte-ignore missing-declaration -->
+  <video id="player2" class="player hidden" {src} autoplay controls></video>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    width: 100%;
+    height: 100%;
+    background-color: black;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  .player {
+    width: 100%;
+    height: 100%;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .hidden {
+    display: none;
+  }
 </style>
